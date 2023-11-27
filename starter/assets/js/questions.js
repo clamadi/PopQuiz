6ab12e3258cdb228f11662dc027fcfc38e5c98a3
CLAmadi <chiamakaamadi93@gmail.com>
2023-11-27 19:38:47 +0000

    created PopQuiz questionniare

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+        q: "The following are semantic HTML elements except?",
+        ansChoices: ["<header>", "<span>", "<article>", "<footer>"],
+        answer: "<span>"
+    },
+    {
+        q: "In Javascript, an Array is said to be _______ ?",
+        ansChoices: ["Zero-based", "Double-based", "All of the Above", "None of the Above"],
+        answer: "Zero-based"
+    },
+    {
+        q: "Which of the following keywords is used to define a variable in Javascript?",
+        ansChoices: ["Var", "Let", "All of the Above", "None of the Above"],
+        answer: "All of the Above"
+    },
+
+    {
+        q: "How can a datatype be declared to be a constant type?",
+        ansChoices: ["const", "var", "let", "constant"],
+        answer: "const"
+    },
+
+    {
+        q: "Which of the following methods is used to access HTML elements using Javascript?",
+        ansChoices: ["getElementbyId", "getElemensByCassName()", "Both", "None of the Above"],
+        answer: "Both"
+    },
+
+    {
+        q: "The following methods can be used to display data in some form using Javascript Except?",
+        ansChoices: ["document.write()", "console.log()", "window.alert()", "None of the Above"],
+        answer: "None of the Above"
+    },
+
+    {
+        q: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        ansChoices: ["Throws an Error", "Ignores the Statement", "Gives Warning", "None of the Above"],
+        answer: "Ignores the Satement"
+    },
+
+    {
+        q: "Javascript is an _______ language?",
+        ansChoices: ["Object Oriented", "Object Based", "Procedural", "None of the Above"],
+        answer: "Object Oriented"
+    },
+];
